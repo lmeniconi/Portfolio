3f41c871b32fa0cb75aecdda8b3b6f13d7e0ba9b
nxx <luciano.meniconi.r@gmail.com>
2022-01-08 15:19:45 -0300

    google analytics tracker

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
-Vue.use(VueGtag, {
-  config: { id: 'G-X37L8E9MF5' },
-})
+export default ({ app }) => {
+  Vue.use(
+    VueGtag,
+    {
+      config: { id: 'G-X37L8E9MF5' },
+      appName: 'Portfolio',
+    },
+    app.router
+  )
+}
